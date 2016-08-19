355e7e4fe8828db4f78add09ed764aa76f49694c
Brian Clifton <brian@clifton.me>
2016-08-18 17:58:09 -0700

    Updates to history page: - Style refresh - Page now breaks history items down by date (sorted desc) - shortened location shown to "domain.tld" - page is now localized (including formatting dates) - Added working "Clear browsing data button" - History is now searchable
    Fixes https://github.com/brave/browser-laptop/issues/441

--- js/about/history.js
+++ js/about/history.js
@@ -4,11 +4,14 @@
 
 // Note that these are webpack requires, not CommonJS node requiring requires
 const React = require('react')
-const Immutable = require('immutable')
-const Sticky = require('react-stickynode')
 const ImmutableComponent = require('../components/immutableComponent')
+const Immutable = require('immutable')
+const urlutils = require('../lib/urlutil.js')
 const messages = require('../constants/messages')
+const settings = require('../constants/settings')
 const aboutActions = require('./aboutActions')
+const getSetting = require('../settings').getSetting
+const Button = require('../components/button')
 
 const ipc = window.chrome.ipc
 
@@ -36,31 +39,23 @@ class HistoryItem extends ImmutableComponent {
         <span>&nbsp;(<span data-l10n-id='partitionNumber' data-l10n-args={JSON.stringify(l10nArgs)} />)</span>
     }
 
-    var className = 'listItem'
-
-    // If the history item is in the selected folder, show
-    // it as selected
-    if (this.props.inSelectedFolder) {
-      className += ' selected'
-    }
-
     return <div role='listitem'
-      className={className}
+      className='listItem'
       onContextMenu={aboutActions.contextMenu.bind(this, this.props.history.toJS(), 'history')}
       data-context-menu-disable
       onDoubleClick={this.navigate.bind(this)}>
-    {
-      this.props.history.get('customTitle') || this.props.history.get('title')
-      ? <span className='aboutListItem' title={this.props.history.get('location')}>
-        <span className='aboutItemTitle'>{this.props.history.get('customTitle') || this.props.history.get('title')}</span>
+      <span className='aboutListItem' title={this.props.history.get('location')}>
+        <span className='aboutItemDate'>{new Date(this.props.history.get('lastAccessedTime')).toLocaleTimeString()}</span>
+        <span className='aboutItemTitle'>
+        {
+          this.props.history.get('customTitle') || this.props.history.get('title')
+          ? this.props.history.get('customTitle') || this.props.history.get('title')
+          : this.props.history.get('location')
+        }
+        </span>
         {partitionNumberInfo}
-        <span className='aboutItemSeparator'>-</span><span className='aboutItemLocation'>{this.props.history.get('location')}</span>
+        <span className='aboutItemLocation'>{urlutils.getHostname(this.props.history.get('location'), true)}</span>
       </span>
-      : <span className='aboutListItem' title={this.props.history.get('location')}>
-        <span>{this.props.history.get('location')}</span>
-        {partitionNumberInfo}
-      </span>
-    }
     </div>
   }
 }
@@ -76,27 +71,71 @@ class HistoryList extends ImmutableComponent {
   }
 }
 
+class HistoryDay extends ImmutableComponent {
+  render () {
+    return <div>
+      <div className='sectionTitle historyDayName'>{this.props.date}</div>
+      <HistoryList history={this.props.entries} />
+    </div>
+  }
+}
+
+class GroupedHistoryList extends ImmutableComponent {
+  getDayString (locale, item) {
+    return new Date(item.get('lastAccessedTime'))
+      .toLocaleDateString(locale, { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' })
+  }
+  groupEntriesByDay (locale) {
+    const reduced = this.props.history.reduce((previousValue, currentValue, currentIndex, array) => {
+      const result = currentIndex === 1 ? [] : previousValue
+      if (currentIndex === 1) {
+        const firstDate = this.getDayString(locale, currentValue)
+        result.push({date: firstDate, entries: [previousValue]})
+      }
+      const date = this.getDayString(locale, currentValue)
+      const dateIndex = result.findIndex((entryByDate) => entryByDate.date === date)
+      if (dateIndex !== -1) {
+        result[dateIndex].entries.push(currentValue)
+      } else {
+        result.push({date: date, entries: [currentValue]})
+      }
+      return result
+    })
+    if (reduced) return reduced
+    return []
+  }
+  render () {
+    const defaultLanguage = this.props.languageCodes.find((lang) => lang.includes(navigator.language)) || 'en-US'
+    const userLanguage = getSetting(settings.LANGUAGE, this.props.settings)
+    return <list className='historyList'>
+    {
+      this.groupEntriesByDay(userLanguage || defaultLanguage).map((groupedEntry) =>
+        <HistoryDay date={groupedEntry.date} entries={groupedEntry.entries} />)
+    }
+    </list>
+  }
+}
+
 class AboutHistory extends React.Component {
   constructor () {
     super()
-    this.onChangeSelectedEntry = this.onChangeSelectedEntry.bind(this)
     this.onChangeSearch = this.onChangeSearch.bind(this)
     this.onClearSearchText = this.onClearSearchText.bind(this)
+    this.clearBrowsingDataNow = this.clearBrowsingDataNow.bind(this)
     this.state = {
       history: Immutable.Map(),
-      selectedEntry: 0,
-      search: ''
+      search: '',
+      settings: Immutable.Map(),
+      languageCodes: Immutable.Map()
     }
     ipc.on(messages.HISTORY_UPDATED, (e, detail) => {
-      this.setState({
-        history: Immutable.fromJS(detail && detail.history || {})
-      })
+      this.setState({ history: Immutable.fromJS(detail && detail.history || {}) })
     })
-  }
-  onChangeSelectedEntry (id) {
-    this.setState({
-      selectedEntry: id,
-      search: ''
+    ipc.on(messages.SETTINGS_UPDATED, (e, settings) => {
+      this.setState({ settings: Immutable.fromJS(settings || {}) })
+    })
+    ipc.on(messages.LANGUAGE, (e, {languageCodes}) => {
+      this.setState({ languageCodes })
     })
   }
   onChangeSearch (evt) {
@@ -109,16 +148,45 @@ class AboutHistory extends React.Component {
       search: ''
     })
   }
+  searchedSiteDetails (searchTerm, siteDetails) {
+    return siteDetails.filter((siteDetail) => {
+      const title = siteDetail.get('customTitle') + siteDetail.get('title') + siteDetail.get('location')
+      return title.match(new RegExp(searchTerm, 'gi'))
+    })
+  }
+  historyDescendingOrder () {
+    return this.state.history.filter((site) => site.get('tags').isEmpty())
+      .sort((left, right) => {
+        if (left.get('lastAccessedTime') < right.get('lastAccessedTime')) return 1
+        if (left.get('lastAccessedTime') > right.get('lastAccessedTime')) return -1
+        return 0
+      }).slice(-500)
+  }
+  clearBrowsingDataNow () {
+    aboutActions.clearBrowsingDataNow()
+  }
   render () {
     return <div className='siteDetailsPage'>
-      <h2 data-l10n-id='history' />
+      <div data-l10n-id='history' className='sectionTitle' />
+      <input type='text' className='searchInput' id='historySearch' value={this.state.search} onChange={this.onChangeSearch} data-l10n-id='historySearch' />
+      {
+        this.state.search
+        ? <span onClick={this.onClearSearchText} className='fa fa-close searchInputClear'></span>
+        : null
+      }
+      <Button l10nId='clearBrowsingDataNow' className='primaryButton clearBrowsingDataButton' onClick={this.clearBrowsingDataNow} />
 
       <div className='siteDetailsPageContent'>
-        <Sticky enabled top={10}>
-          <HistoryList history={this.state.history.filter((site) => site.get('tags').isEmpty()).slice(-500)}
-            onChangeSelectedEntry={this.onChangeSelectedEntry}
-            selectedEntry={this.state.selectedEntry} />
-        </Sticky>
+      {
+        this.state.search
+        ? <list className='historyList'>
+          <HistoryList history={this.searchedSiteDetails(this.state.search, this.state.history)} />
+        </list>
+        : <GroupedHistoryList
+          languageCodes={this.state.languageCodes}
+          settings={this.state.settings}
+          history={this.historyDescendingOrder()} />
+       }
       </div>
     </div>
   }
